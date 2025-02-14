4828d6e840acc05c3f218e9ffaf20b2a7fdecf9f
bunsal.infinity <bunsal141.infinity@gmail.com>
2025-02-14 08:26:27 +0700

    [happy-birthday] - initial firework and wishes

--- script/main.js
+++ script/main.js
@@ -4,7 +4,7 @@ let isPlaying = false;
 
 // Import the data to customize and insert them into page
 const fetchData = () => {
-  fetch("customize.json")
+  fetch("wishes.json")
     .then((data) => data.json())
     .then((data) => {
       dataArr = Object.keys(data);
